d40fea75fe85a11d5639e16f007edab4ebc4ec4d
TerseSquirrel <Support@TerseSquirrel.com>
2025-12-30 14:12:27 -0700

    Update to work with ZimWiki v0.76.3+

--- recentpages.py
+++ recentpages.py
@@ -4,26 +4,22 @@
 # Search instantly as you type. Edvard Rejthar
 # https://github.com/e3rd/zim-plugin-instantsearch
 #
-from collections import defaultdict, OrderedDict
-import copy
-import gobject
-import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+
 import logging
-from pprint import pprint
+from collections import OrderedDict
+
+from gi.repository import Gtk, Pango
+
 from zim.actions import action
+from zim.gui.mainwindow import MainWindowExtension
 from zim.gui.widgets import Dialog
 from zim.gui.widgets import InputEntry
 from zim.history import HistoryList
 from zim.history import HistoryPath
 from zim.notebook import Path
 from zim.plugins import PluginClass
-from zim.plugins import WindowExtension
-from zim.plugins import extends
-from zim.search import *
-from zim.index import IndexPath
-from copy import deepcopy
-import sys
-import inspect
 
 logger = logging.getLogger('zim.plugins.recentpages')
 
@@ -40,8 +36,7 @@ Show the recently viewed pages in a popup window.
     }
 
 
-@extends('MainWindow')
-class RecentPagesMainWindowExtension(WindowExtension):
+class RecentPagesMainWindowExtension(MainWindowExtension):
 
     uimanager_xml = '''
     <ui>
@@ -56,21 +51,21 @@ class RecentPagesMainWindowExtension(WindowExtension):
     '''
 
 
-    gui = "";
+    gui = None
 
     @action(_('_Recent Pages'), accelerator='<ctrl><shift>r') # T: menu item
     def recentpages(self):
 
         #init
         #TODO: Get Items from pathbar
-        self.history = self.window.ui.history.get_recent()
+        self.history = self.window.history.get_recent()
 
         # preferences
         # self.plugin.preferences['keystroke_delay']
 
 
         # Gtk
-        self.gui = Dialog(self.window.ui, _('Recent Pages'), buttons=None, defaultwindowsize=(300, -1))
+        self.gui = Dialog(self.window, _('Recent Pages'), buttons=None, defaultwindowsize=(400, -1))
 
         (px, py) = self.window.get_position()
         (pw, ph) = self.window.get_size()
@@ -82,46 +77,46 @@ class RecentPagesMainWindowExtension(WindowExtension):
         #self.inputEntry = InputEntry()
         #self.inputEntry.connect('key_press_event', self.move)
         #self.inputEntry.connect('changed', self.change) # self.change is needed by GObject or something
-        #self.gui.vbox.pack_start(self.inputEntry, False)
+        #self.gui.vbox.pack_start(self.inputEntry, False, False, 0)
 
 
-        self.listmodel = gtk.ListStore(str)
+        self.listmodel = Gtk.ListStore(str)
 
         # My own model for creating dialog labels and later retrieving them
         self.model = OrderedDict()
         for page in self.history:
-            ky = str(page)
-            ky = ky[1:-1]
-            ky = ky.split(":")
-            ky = ky[-1].strip()
+            # Get the page name directly from the page object
+            ky = page.name if hasattr(page, 'name') else str(page)
+            # If it's a full path, get just the last part
+            if ':' in ky:
+                ky = ky.split(':')[-1].strip()
             self.model[ky] = page
 
         # populate the treeview
         for row in self.model.keys():
             self.listmodel.append([row])
-        self.treeview = gtk.TreeView(model=self.listmodel)
+        self.treeview = Gtk.TreeView(model=self.listmodel)
         self.treeview.set_headers_visible(False)
         # cellrenderer to render the text
-        cell = gtk.CellRendererText()
+        cell = Gtk.CellRendererText()
+        cell.set_property('ellipsize', Pango.EllipsizeMode.NONE)  # Don't truncate with ellipsis
         # the text in the first column should be in boldface
         # the column is created
-        col = gtk.TreeViewColumn("", cell, text=0)
+        col = Gtk.TreeViewColumn("", cell, text=0)
         # and it is appended to the treeview
         self.treeview.append_column(col)
 
         self.treeview.connect("row-activated", self.on_row_activated)
 
-        self.gui.vbox.pack_start(self.treeview, False)
+        self.gui.vbox.pack_start(self.treeview, True, True, 0)
 
         self.gui.show_all()
 
 
-    def on_row_activated(self, path, view_column,user_param):
+    def on_row_activated(self, treeview, path, view_column):
         #self.gui.move(600, 600)
         #ui.open_page(path)
         (tm, it) = self.treeview.get_selection().get_selected()
         ky = tm[it][0]
-        self.window.ui.open_page(self.model[ky])
+        self.window.open_page(self.model[ky])
         self.gui.destroy()
-        print "on_change"
-        print selection
